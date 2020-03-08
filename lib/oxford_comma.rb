9bdc20b9e96f91831bbd8d08aa604977f19fe8c5
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-08 22:41:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     array.join
     elsif array.length == 2 
     array.join " and "
-  else array.length > 2 
+  else
     array.push[-1] = "and #{array[-1]}"
     array.join","
   
